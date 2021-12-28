cc8a2e4f4b67ed544697dadf30f78ae1af486131
Salma <salmahalloumi98@gmail.com>
2021-12-28 19:09:52 +0100

    fixed route

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
 	res.json({ message: "Hello! Welcome to Salma's Website!" });
 });
 
